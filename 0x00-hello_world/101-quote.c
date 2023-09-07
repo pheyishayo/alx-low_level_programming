cd9c319a80bff6071eedea565ec3ea374c9f5c19
pheyishayo <amzatsayi@yahoo.com>
2023-09-07 11:45:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+/**
+ * main - Start of the project
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(1, message, 59); 
+return (0); 
+}
+
