68b804647505b88ce153cf9bf33f72af7a891aa9
pheyishayo <amzatsayi@yahoo.com>
2023-09-08 11:33:59 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting the program
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9' ; i++)
+	putchar (i);
+	putchar ('\n');
+	return (0);
+}
