48d96a85efcd8cb75b31f1c3d43d741bf1b1364d
pheyishayo <amzatsayi@yahoo.com>
2023-09-08 10:51:36 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Start of the main function of the  project
+ *
+ * Return: This is 0
+*/
+int main(void)
+{
+	char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+for (i = 'A' ; i <= 'Z' ; i++)
+putchar(i);
+putchar ('\n');
+return (0);
+}
