b5e34f9613c73fa6c39a667a30ffcc2473015f16
pheyishayo <amzatsayi@yahoo.com>
2023-09-09 12:09:14 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting of the project
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9' ; i++)
+putchar (i + '0');
+putchar ('\n');
+return (0);
+}
