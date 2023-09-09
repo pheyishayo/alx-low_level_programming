4663c147a602fc15b6507a445831cfdfe18e3de3
pheyishayo <amzatsayi@yahoo.com>
2023-09-09 13:03:31 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting of the project
+ *
+ * Return: Always to 0
+*/
+int main(void)
+{
+	char i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return(0);
+}
