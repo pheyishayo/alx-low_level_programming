d0764733c6955c9fd2f8c8668d36c6b827bf90e3
pheyishayo <amzatsayi@yahoo.com>
2023-09-09 14:24:39 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Starting of a new project
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int k;
+for (k = 0 ; k < 10 ; k++)
+	{
+putchar (k + '0');
+if (k + '0')
+putchar (',');
+putchar (' ');
+	}
+return (0);
+}
