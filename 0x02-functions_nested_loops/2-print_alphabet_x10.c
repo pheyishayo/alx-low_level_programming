33ce5503686a4488df4bba59ebb07f999b9de53d
pheyishayo <amzatsayi@yahoo.com>
2023-09-12 11:10:21 -0700

    20x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10x
+ *
+ * Return:0
+*/
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+for (a = 0 ; a < 10 ; a++)
+{
+for (b = 'a' ; b <= 'z' ; b++)
+_putchar(b);
+}
+
+_putchar('\n');
+}
