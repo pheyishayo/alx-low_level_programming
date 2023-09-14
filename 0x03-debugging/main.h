522909cf4701cbda803d7038c7905bc1121c1576
pheyishayo <amzatsayi@yahoo.com>
2023-09-14 15:44:12 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a. int b_ int c)
+pid print_remaining _days(int month int day int year)
+int convert_day(int month,_intiday)
+
+#endif /* MAIN_H */
