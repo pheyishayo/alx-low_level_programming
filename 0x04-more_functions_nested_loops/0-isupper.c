a1d16267654e02f91cbef1ae72b1effabd2fa662
pheyishayo <amzatsayi@yahoo.com>
2023-09-14 06:24:10 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /*
  * -isupper -checks for uppercase characters
- *  @c: character to be checked
+ *  @c: Character to be checked
  *  Return: 1 if c is uppercase 0 on failure
 */
 int _isupper(int c)
