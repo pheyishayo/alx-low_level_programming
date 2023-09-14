c540353034c265dcea97001e6abd726ccc23e0a7
pheyishayo <amzatsayi@yahoo.com>
2023-09-14 08:09:46 -0700

    4print

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,17 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+void print_most_numbers(void);
+
 
 #endif
