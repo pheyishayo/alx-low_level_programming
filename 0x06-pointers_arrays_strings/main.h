5db7dbe6699bd8ffee1055be3c0f021b8232b732
pheyishayo <amzatsayi@yahoo.com>
2023-09-20 02:47:48 -0700

    2strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
