3034dc5aee7aceda1267a39d96de19f64cade0f1
pheyishayo <amzatsayi@yahoo.com>
2023-09-28 11:47:16 -0700

    pende

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,38 +3,58 @@
 /**
  * is_palindrome_recursive - Helper function to check if
  * a string is a palindrome recursively.
- * @s: The string to be checked
- * @a: Starting index of the string
- * @z: Ending index of the string
+ * @str: The string to be checked
+ * @start: Starting index of the string
+ * @end: Ending index of the string
  *
  * Return: 1 if the string is a palindrome, otherwise 0
  */
-int is_palindrome_recursive(char *s, int a, int z)
+int is_palindrome_recursive(char *str, int start, int end)
 {
-if (a >= z)
+if (start >= end)
 {
 return (1);
 }
-if (s[a] != s[z])
+
+if (str[start] != str[end])
 {
 return (0);
 }
-return (is_palindrome_recursive(s, a + 1, z - 1));
+
+return (is_palindrome_recursive(str, start + 1, end - 1));
 }
 
 /**
- * is_palindrome - Function that returns 1 if a string is a palindrome
- * @s: The string to be checked
+ * get_string_length_recursive - Returns the length of a string recursively.
+ * @str: The string to calculate the length of
  *
- * Return: 1 if the string is a palindrome, otherwise 0
+ * Return: The length of the string
  */
-int is_palindrome(char *s)
+int get_string_length_recursive(char *str)
 {
-int leng = 0;
+if (*str == '\0')
+{
+return (0);
+}
 
-while (s[leng] != '\0')
+return (1 + get_string_length_recursive(str + 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @str: The string to be checked
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *str)
 {
-leng++;
+int length;
+if (*str == '\0')
+{
+return (1);
 }
-return (is_palindrome_recursive(s, 0, leng - 1));
+
+length = get_string_length_recursive(str);
+return (is_palindrome_recursive(str, 0, length - 1));
 }
+
