cea91a17436ebc51552e75a94f28bd9962ea0589
pheyishayo <amzatsayi@yahoo.com>
2023-10-19 14:24:02 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 list_t *new_node;
 int q;
 
-for (q = 0; lis_str[q] != '\0'; q++)
+for (q = 0; str[q] != '\0'; q++)
 ;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
@@ -32,4 +32,3 @@ new_node->next = *head;
 *head = new_node;
 return (*head);
 }
-
