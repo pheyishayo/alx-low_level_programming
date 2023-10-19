29ba8b59af899adbf7c9dc5bc72c3d7f13da0c86
pheyishayo <amzatsayi@yahoo.com>
2023-10-19 15:24:03 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,20 @@
 section .data
-hello db "Hello, Holberton",0
+    hello db "Hello, Holberton",0
+    format db "%s", 10, 0
 
 section .text
-global main
+    global main
 
-extern printf
+    extern printf, exit
+
 main:
-push rbp
-mov rdi, hello
-call printf
-pop rbp
-
-ret
+    push rbp
+    mov rdi, format
+    mov rsi, hello
+    call printf
+    add rsp, 8  ; Adjust the stack after the call (64-bit calling convention)
+    
+    ; Exit the program
+    mov rax, 60
+    xor rdi, rdi
+    syscall
