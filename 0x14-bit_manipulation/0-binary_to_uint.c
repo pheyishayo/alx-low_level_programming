74ba4b0b46ff3372a90cee5c8ae416ee6dfbac50
pheyishayo <amzatsayi@yahoo.com>
2023-10-26 08:35:52 -0700

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,6 @@
 /**
  * binary_to_uint -function that converts a binary number to an unsigned int
  * @b: char string pointing 0 and 1 char
- * 
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
@@ -21,5 +20,5 @@ if (b[d] != '0' && b[d] != '1')
 return (0);
 convertedValue = (convertedValue * 2) + (b[d] - '0');
 }
-return convertedValue;
+return (convertedValue);
 }
